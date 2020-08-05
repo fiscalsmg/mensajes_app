0c27543032c39a16279c0ba9512d5ab0388a076f
Fiscal <fiscalguadalupe422@gmail.com>
2020-08-05 01:07:39 -0500

    Creacion de la consulta Insert en el metodo CreateMessageDB

--- src/main/java/com/mycompany/mensajes_app/MessageDAO.java
+++ src/main/java/com/mycompany/mensajes_app/MessageDAO.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *Data Acces Object
  * Se crean 4 metodos que no van a devolver nada
@@ -13,8 +17,28 @@ package com.mycompany.mensajes_app;
 public class MessageDAO {
     /*Crear mensajes*/
     public static void createMessageDB(Message message){
-        
+        Conexion dbConect=new Conexion();
+        try(Connection conexion=dbConect.get_connection()) {
+            //Try para generar la insercion de los datos
+            PreparedStatement ps=null;
+            try {
+                String query="INSERT INTO `mensajes` (mensaje, autor_mensaje) VALUES (?,?)";
+                ps=conexion.prepareStatement(query);
+                //parametros que seran enviados a la base de datos
+                ps.setString(1,message.getMessage());//el mensaje es el atributo mensaje del objeto mensaje
+                ps.setString(2, message.getAuthor_message());
+                ps.executeUpdate();
+                System.out.println("Mensaje Creado");
+                
+            } catch (SQLException e) {
+                 System.out.println(e);
+            }
+            
+        } catch (SQLException e) {
+             System.out.println(e);
+        }
     }
+    
     public static void readMessageDB(){
         
     }
