b09c628460bf1a5677a7ab4d6febb8c0d10caa30
Fiscal <fiscalguadalupe422@gmail.com>
2020-08-05 01:08:41 -0500

    Creacion de objetos para el registro del mensaje (Escribir y nombre)

--- src/main/java/com/mycompany/mensajes_app/MessageService.java
+++ src/main/java/com/mycompany/mensajes_app/MessageService.java
@@ -5,22 +5,42 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.util.Scanner;
+
 /**
- *Metodos que va recibir el menu y se van a conectar con la capa DAO
+ * Metodos que va recibir el menu y se van a conectar con la capa DAO
+ *
  * @author Fiscal
+ *
+ * Capara se encarga de preguntar los datos para enviarlo a la capa de mensajes
  */
 public class MessageService {
-    public static void createMessage(){
-        
+
+    public static void createMessage() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensjae");
+        String mensaje = sc.nextLine();
+
+        System.out.println("Tu nombre");
+        String nombre = sc.nextLine();
+
+        Message registro = new Message();
+        registro.setMessage(mensaje);
+        registro.setAuthor_message(nombre);
+
+        //Enviar el objeto a DAO
+        MessageDAO.createMessageDB(registro);
     }
-    
-    public static void listMessage(){
-        
+
+    public static void listMessage() {
+
     }
-    public static void deleteMessage(){
-        
+
+    public static void deleteMessage() {
+
     }
-    public static void editMessage(){
-        
+
+    public static void editMessage() {
+
     }
 }
